edd362be57b0a5510add76fd0951f8039b1490f7
rekafekili <rekafekili@gmail.com>
2020-07-16 17:41:37 +0900

    Solve 1010

---
+++ src/baekjoon/BOJ_1010.java
@@ -0,0 +1,40 @@
+package baekjoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/1010
+public class BOJ_1010 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int testCase = sc.nextInt();
+
+        for (int i = 0; i < testCase; i++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+
+            if (N == M) {
+                System.out.println(1);
+            } else if (N == 1) {
+                System.out.println(M);
+            } else {
+                solve(N, M);
+            }
+        }
+    }
+
+    private static void solve(int n, int m) {
+        BigInteger result = factorial(m).divide(factorial(m - n).multiply(factorial(n)));
+        System.out.println(result);
+    }
+
+    private static BigInteger factorial(int d) {
+        BigInteger result = BigInteger.valueOf(1);
+        for (int i = d; i > 0; i--) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+
+        return result;
+    }
+}
--- src/baekjoon/BOJ_14501.java
+++ src/baekjoon/BOJ_14501.java
@@ -1,4 +1,4 @@
-package week1;
+package baekjoon;
 
 import java.util.Scanner;
 
