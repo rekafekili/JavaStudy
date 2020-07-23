b69b141706023d5729b92fa7b6cf541dfb151067
Rekafekili <rekafekili@gmail.com>
2020-07-23 22:14:30 +0900

    Add this week Problems

--- src/baekjoon/BOJ_1114.java
+++ src/baekjoon/BOJ_1114.java
@@ -3,6 +3,6 @@ package baekjoon;
 // https://www.acmicpc.net/problem/1114
 public class BOJ_1114 {
     public static void main(String[] args) {
-
+        
     }
 }
---
+++ src/baekjoon/BOJ_9372.java
@@ -0,0 +1,5 @@
+package baekjoon;
+
+// https://www.acmicpc.net/problem/9372
+public class BOJ_9372 {
+}
