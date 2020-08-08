13c4b1cffb9b499fd50353e6dae8cababa2ef775
rekafekili <rekafekili@gmail.com>
2020-08-08 23:58:12 +0900

    temp

---
+++ src/baekjoon/BOJ_15649.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BOJ_15649 {
+    private static ArrayList<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        boolean[] started = new boolean[N + 1];
+        boolean[] checkArray = new boolean[N + 1];
+
+
+    }
+}
