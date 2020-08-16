641bdaec4c018afb9d012b54a552506fb3a83ccc
Rekafekili <rekafekili@gmail.com>
2020-08-16 14:02:36 +0900

    temp

--- src/baekjoon/BOJ_2869.java
+++ src/baekjoon/BOJ_2869.java
@@ -1,8 +1,14 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 // https://www.acmicpc.net/problem/2869
 public class BOJ_2869 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int X = sc.nextInt();
+
+        System.out.println();
     }
 }
