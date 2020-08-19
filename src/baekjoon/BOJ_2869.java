d8e5cace5a6c7564b8e87d80931f16251aaef1ed
rekafekili <rekafekili@gmail.com>
2020-08-19 20:49:09 +0900

    Solved 2869

--- src/baekjoon/BOJ_2869.java
+++ src/baekjoon/BOJ_2869.java
@@ -3,25 +3,21 @@ package baekjoon;
 import java.io.*;
 import java.util.Scanner;
 
+/**
+ * Solved - Repeat
+ */
+
+// https://www.acmicpc.net/problem/2869
 public class BOJ_2869 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        String input = br.readLine();
-        int A = Integer.parseInt(input.split(" ")[0]);
-        int B = Integer.parseInt(input.split(" ")[1]);
-        int V = Integer.parseInt(input.split(" ")[2]);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int V = sc.nextInt();
 
-        int day;
-        if(V==A) {
-            day = 1;
-        } else {
-            day = V-B;
+        int day = (V - B - 1) / (A - B) + 1;
 
-        }
-
-        bw.write("" + day);
-        bw.flush();
+        System.out.println(day);
     }
 }
