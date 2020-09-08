26802e3843fd8bd283b13adad12199c731ce3a5b
rekafekili <rekafekili@gmail.com>
2020-09-08 23:57:24 +0900

    Tmp

---
+++ src/baekjoon/BOJ_3009.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class BOJ_3009 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] x = new int[1001];
+    }
+}
---
+++ src/baekjoon/BOJ_4153.java
@@ -0,0 +1,9 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class BOJ_4153 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
