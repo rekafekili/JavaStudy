d1fbafed5983653b79c4ce1b2e5709f64c8f4209
rekafekili <rekafekili@gmail.com>
2020-09-10 00:02:13 +0900

    Solved 4153

--- src/baekjoon/BOJ_4153.java
+++ src/baekjoon/BOJ_4153.java
@@ -4,11 +4,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
+/**
+ * Solved
+ */
+
+// https://www.acmicpc.net/problem/4153
 public class BOJ_4153 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        while(true) {
+        while (true) {
             ArrayList<Integer> numbers = new ArrayList<>();
 
             for (int i = 0; i < 3; i++) {
@@ -18,10 +23,10 @@ public class BOJ_4153 {
                 }
             }
 
-            if(numbers.isEmpty()) {
+            if (numbers.isEmpty()) {
                 break;
             } else {
-                if(isRightTriangle(numbers)) {
+                if (isRightTriangle(numbers)) {
                     System.out.println("right");
                 } else {
                     System.out.println("wrong");
@@ -37,10 +42,14 @@ public class BOJ_4153 {
         int b = numbers.get(1);
         int c = numbers.get(2);
 
-        if( a + b >= c ) {
+        if (a + b >= c) {
+            int a2 = (int) Math.pow(a, 2);
+            int b2 = (int) Math.pow(b, 2);
+            int c2 = (int) Math.pow(c, 2);
 
+            return (c2 == a2 + b2);
         } else {
-            return false
+            return false;
         }
     }
 }
