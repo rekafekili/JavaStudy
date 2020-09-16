39e60b4cd21a11bcdd83b53f8946edd0b3d4eac4
rekafekili <rekafekili@gmail.com>
2020-09-16 23:11:49 +0900

    Solved 1929

--- src/baekjoon/BOJ_1929.java
+++ src/baekjoon/BOJ_1929.java
@@ -2,30 +2,47 @@ package baekjoon;
 
 import java.util.Scanner;
 
+/**
+ * Solved
+ */
+
+// https://www.acmicpc.net/problem/1929
 public class BOJ_1929 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int M = sc.nextInt();
-        int N = sc.nextInt();
+        int M_start = sc.nextInt();
+        int N_end = sc.nextInt();
 
-        for (int i = M; i <= N; i++) {
-            if (isPrime(i)) {
-                System.out.println(i);
-            }
-        }
-    }
+        // false : 소수, true : 소수가 아님
+        boolean[] numbers = new boolean[N_end + 1];
 
-    private static boolean isPrime(int num) {
-        if (num == 1) {
-            return false;
-        } else {
-            for (int i = 2; i < num; i++) {
-                if (num % i == 0) {
-                    return false;
+        numbers[1] = true;
+        for (int i = 1; i <= N_end; i++) {
+            if(!numbers[i]) {
+                // 배수 찾기
+                int start = (int) Math.pow(i, 2);
+                if(start > 1000000) {
+                    continue;
+                }
+
+                for (int j = 0; ; j++) {
+                    int current = start + i*j;
+                    if(current > 1000000 || current > N_end) {
+                        break;
+                    } else {
+                        if(!numbers[current]) {
+                            numbers[current] = true;
+                        }
+                    }
                 }
             }
-            return true;
+        }
+
+        for (int i = M_start; i <= N_end; i++) {
+            if(!numbers[i]) {
+                System.out.println(i);
+            }
         }
     }
 }
