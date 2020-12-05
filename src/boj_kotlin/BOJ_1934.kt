181dc3c83f618f8cd01b763f8e6a940a50ac8a56
rekafekili <rekafekili@gmail.com>
2020-12-05 16:30:57 +0900

    Solved 1934

--- src/boj_kotlin/BOJ_1934.kt
+++ src/boj_kotlin/BOJ_1934.kt
@@ -1,5 +1,30 @@
 package boj_kotlin
 
-fun main() {
+import kotlin.math.max
+import kotlin.math.min
 
+/**
+ * Solved
+ *
+ * https://www.acmicpc.net/problem/1934
+ */
+fun main() {
+    val testCase = readLine()!!.toInt()
+
+    for (i in 1..testCase) {
+        val input = readLine()!!
+        val first = input.split(" ")[0].toInt()
+        val second = input.split(" ")[1].toInt()
+
+        var target = max(first, second)
+        var divider = min(first, second)
+
+        while (target % divider != 0) {
+            val mod = target % divider
+            target = divider
+            divider = mod
+        }
+
+        println(divider * (first/divider) * (second/divider))
+    }
 }
