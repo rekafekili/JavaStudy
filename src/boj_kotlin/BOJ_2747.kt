1147965ef94a6be77a87b002fafb79b19ede1ba9
rekafekili <rekafekili@gmail.com>
2021-03-25 11:45:49 +0900

    Solved 2747

---
+++ src/boj_kotlin/BOJ_2747.kt
@@ -0,0 +1,35 @@
+package boj_kotlin
+
+/**
+ * Solved
+ *
+ * https://www.acmicpc.net/problem/2747
+ */
+
+fun main() {
+    val n = readLine()!!.toInt()
+
+    println(getFibonacci(n))
+}
+
+fun getFibonacci(n: Int): Int {
+    return when(n) {
+        0 -> 0
+        1 -> 1
+        2 -> 1
+        else -> {
+            val initF = arrayOf(0, 0, 1)
+            var res = 0
+            for(i in 1 until n) {
+                initF[0] = initF[1]
+                initF[1] = initF[2]
+
+                initF[2] = initF[0] + initF[1]
+
+                res = initF[2]
+            }
+
+            return res
+        }
+    }
+}
