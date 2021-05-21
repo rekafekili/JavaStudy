f002c26710925d2e9824c26f03d9f2975c497362
rekafekili <rekafekili@gmail.com>
2021-05-21 14:27:08 +0900

    Solved 9020

--- src/baekjoon/BOJ_9020.java
+++ src/baekjoon/BOJ_9020.java
@@ -1,7 +1,44 @@
 package baekjoon;
 
+import java.util.Scanner;
+
+/**
+ * Solved
+ */
+
+// https://www.acmicpc.net/problem/9020
 public class BOJ_9020 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testCase = sc.nextInt();
 
+        for (int i = 0; i < testCase; i++) {
+            int target = sc.nextInt();
+
+            int middle = target / 2;
+
+            if (isPrime(middle)) {
+                System.out.println(middle + " " + middle);
+            } else {
+                for (int j = 1; j < middle; j++) {
+                    if (isPrime(middle - j)) {
+                        if (isPrime(middle + j)) {
+                            System.out.println((middle - j) + " " + (middle + j));
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
